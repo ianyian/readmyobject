07590fcb2d6a274e6d1b62757f08aca7e5379a37
XJ <ianyian@gmail.com>
2025-12-24 03:30:33 +0800

    Initial Commit

---
+++ readmyobject/readmyobjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  readmyobjectApp.swift
+//  readmyobject
+//
+//  Created by XJ on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct readmyobjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ readmyobjectTests/readmyobjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  readmyobjectTests.swift
+//  readmyobjectTests
+//
+//  Created by XJ on 24/12/2025.
+//
+
+import Testing
+@testable import readmyobject
+
+struct readmyobjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
